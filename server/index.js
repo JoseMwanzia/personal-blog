8d4e32b3205f99e9dee58f81ebb67b371f2afa27
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-04 23:58:29 +0300

    chore: update the server logic

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const posts = require('../files/posts')
 
+
+
+app.use(express.static('src/views'));
 app.set('view engine', 'pug');
 app.set('views', './src/views')
 
-app.get('/home',  (req, res) => {
-    res.render('index.pug')
+app.get('/', (req, res) => {
+    res.render('index.pug', {posts})
 })
+
+// app.get('/home', (req, res) => {
+//     res.render('index.pug', {posts})
+// })
 
 app.listen(3000, () => { console.log('App listening on port 3000') })
