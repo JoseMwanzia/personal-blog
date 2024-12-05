1b07e7c389b949a462e4cdd91a6ff256e23313fb
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-05 17:09:50 +0300

    feat: server side logic for public pages

--- server/index.js
+++ server/index.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const app = express();
-const posts = require('../files/posts')
+const fs = require('node:fs')
+const port = 3000;
 
-
-
-app.use(express.static('src/views'));
+app.use(express.static('./src/views'));
 app.set('view engine', 'pug');
 app.set('views', './src/views')
 
-app.get('/', (req, res) => {
-    res.render('index.pug', {posts})
+app.get('/home', async (req, res) => {
+    try {
+        const readDir = fs.readdirSync('./files');
+
+        const articles = readDir.map(article => {
+            const readFiles = fs.readFileSync(`./files/${article}`, 'utf8')
+            return { article, content: JSON.parse(readFiles) }
+        })
+        res.render('index.pug', { articles })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Failed to load the files!')
+    }
 })
 
-// app.get('/home', (req, res) => {
-//     res.render('index.pug', {posts})
-// })
+app.get('/article/:artcleId', async (req, res) => {
+    try {
+        const article_id = req.params.artcleId
 
-app.listen(3000, () => { console.log('App listening on port 3000') })
+        const readDir = await fs.readdirSync('./files');
+
+        const article = readDir.find(async article => {
+            const readFile = await fs.readFileSync(`./files/${article}`, 'utf8')
+            return article_id === JSON.parse(readFile).id
+        })
+
+        const readFile = await fs.readFileSync(`./files/${article}`, 'utf8')
+        res.render('article.pug', { article: await JSON.parse(readFile) })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Failed to load the files')
+    }
+})
+
+app.listen(port, () => { console.log(`App listening on port ${port}`) })
